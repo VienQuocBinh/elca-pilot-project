c8fbbf041e2156567442a4898cca141c9fa7f622
VienQuocBinh <quocbinhvien@gmail.com>
2024-08-01 14:48:07 +0700

    BE: SalaryServiceTest

--- pilot-project-back/src/test/java/vn/elca/training/pilot_project_back/service/SalaryServiceTest.java
+++ pilot-project-back/src/test/java/vn/elca/training/pilot_project_back/service/SalaryServiceTest.java
@@ -30,7 +30,9 @@ import vn.elca.training.pilot_project_back.repository.SalaryRepository;
 import vn.elca.training.pilot_project_back.service.impl.SalaryServiceImpl;
 
 import java.io.File;
+import java.io.IOException;
 import java.math.BigDecimal;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
@@ -242,11 +244,11 @@ class SalaryServiceTest {
         String filePath = util.FileUtil.writeErrorCsvFile("test", HeaderBuild.buildImportSalaryHeader(), new ArrayList<>());
         File file = new File(filePath);
         assertTrue(file.exists());
-        System.out.println(filePath);
         try (MockedStatic<util.FileUtil> mockedFileUtil = mockStatic(util.FileUtil.class)) {
             SalaryFileResult mockResult = mock(SalaryFileResult.class);
             List<model.Salary> validSalaries = new ArrayList<>();
             List<model.SalaryError> errorSalaries = new ArrayList<>();
+            List<String> existAvsNumberInPeriod = new ArrayList<>();
             for (int i = 0; i < 5; i++) {
                 validSalaries.add(model.Salary.builder()
                         .id((long) i)
@@ -275,17 +277,100 @@ class SalaryServiceTest {
                         .message("Test error salaries")
                         .build());
             }
+            existAvsNumberInPeriod.add("756.4546.3216.01");
             when(mockResult.getSalaries()).thenReturn(validSalaries); // Mock valid salary list
             when(mockResult.getErrors()).thenReturn(errorSalaries); // Mock errors list
             // Mock processing the CSV file
             mockedFileUtil.when(() -> util.FileUtil.processSalaryCsvFiles(any(File.class))).thenReturn(mockResult);
 
-            when(salaryRepository.findAvsNumbersBetweenDates(any(Date.class), any(Date.class))).thenReturn(new ArrayList<>());
-            when(employerRepository.findByIdeNumber(employer.getIdeNumber())).thenReturn(Optional.of(employer));
+            when(salaryRepository.findAvsNumbersBetweenDates(any(Date.class), any(Date.class))).thenReturn(existAvsNumberInPeriod);
+            when(employerRepository.findByIdeNumber(employer.getIdeNumber())).thenReturn(Optional.empty());
 
             salaryService.processSalaryCsvFilesJob();
-            verify(salaryRepository, times(validSalaries.size())).findAvsNumbersBetweenDates(any(Date.class), any(Date.class));
+            verify(salaryRepository, times(5)).findAvsNumbersBetweenDates(any(Date.class), any(Date.class));
+            verify(salaryRepository).saveAll(anyList());
         }
     }
 
+
+    @Test
+    void testProcessSalaryCsvFilesJob_PathIsNotDirectory() throws IOException {
+        File mockFile = Files.createFile(tempDir.resolve("not_a_dir.csv")).toFile();
+        ReflectionTestUtils.setField(salaryService, "directoryPath", mockFile.getAbsolutePath());
+        salaryService.processSalaryCsvFilesJob();
+        try (MockedStatic<util.FileUtil> mockedFileUtil = mockStatic(util.FileUtil.class)) {
+            mockedFileUtil.verifyNoInteractions();
+            verify(salaryRepository, times(0)).findAvsNumbersBetweenDates(any(Date.class), any(Date.class));
+        }
+    }
+
+    @Test
+    void testProcessSalaryCsvFilesJobStartDateParseException() throws IOException {
+        try (MockedStatic<util.FileUtil> mockedFileUtil = mockStatic(util.FileUtil.class)) {
+            File tempFile = Files.createFile(tempDir.resolve("test.csv")).toFile();
+
+            SalaryFileResult mockResult = mock(SalaryFileResult.class);
+            List<model.Salary> mockSalaries = new ArrayList<>();
+            model.Salary mockSalary = new model.Salary();
+            mockSalary.setStartDate("invalid_date"); // This will cause a ParseException
+            mockSalaries.add(mockSalary);
+            when(mockResult.getSalaries()).thenReturn(mockSalaries);
+            mockedFileUtil.when(() -> util.FileUtil.processSalaryCsvFiles(tempFile)).thenReturn(mockResult);
+
+            // Inject the directory path into the service
+            ReflectionTestUtils.setField(salaryService, "directoryPath", tempDir.toString());
+
+            salaryService.processSalaryCsvFilesJob();
+            mockedFileUtil.verify(() -> util.FileUtil.processSalaryCsvFiles(tempFile));
+        }
+    }
+
+    @Test
+    void testProcessSalaryCsvFilesJobEndDateParseException() throws IOException {
+        try (MockedStatic<util.FileUtil> mockedFileUtil = mockStatic(util.FileUtil.class)) {
+            File tempFile = Files.createFile(tempDir.resolve("test.csv")).toFile();
+
+            SalaryFileResult mockResult = mock(SalaryFileResult.class);
+            List<model.Salary> mockSalaries = new ArrayList<>();
+            model.Salary mockSalary = new model.Salary();
+            mockSalary.setStartDate(simpleDateFormat.format(new Date()));
+            mockSalary.setEndDate("invalid_date"); // This will cause a ParseException
+            mockSalaries.add(mockSalary);
+            when(mockResult.getSalaries()).thenReturn(mockSalaries);
+            mockedFileUtil.when(() -> util.FileUtil.processSalaryCsvFiles(tempFile)).thenReturn(mockResult);
+
+            // Inject the directory path into the service
+            ReflectionTestUtils.setField(salaryService, "directoryPath", tempDir.toString());
+
+            salaryService.processSalaryCsvFilesJob();
+            mockedFileUtil.verify(() -> util.FileUtil.processSalaryCsvFiles(tempFile));
+        }
+    }
+
+    @Test
+    void testProcessSalaryCsvFilesJobValid() throws IOException {
+        try (MockedStatic<util.FileUtil> mockedFileUtil = mockStatic(util.FileUtil.class)) {
+            File tempFile = Files.createFile(tempDir.resolve("test.csv")).toFile();
+
+            SalaryFileResult mockResult = mock(SalaryFileResult.class);
+            List<model.Salary> mockSalaries = new ArrayList<>();
+            mockSalaries.add(model.Salary.builder()
+                    .startDate(simpleDateFormat.format(new Date()))
+                    .endDate(simpleDateFormat.format(new Date()))
+                    .avsAmount(String.valueOf(123.1))
+                    .afAmount(String.valueOf(123.1))
+                    .acAmount(String.valueOf(123.1))
+                    .employerIdeNumber("756.4223.1234.33")
+                    .build());
+            when(mockResult.getSalaries()).thenReturn(mockSalaries);
+            when(employerRepository.findByIdeNumber(any(String.class))).thenReturn(Optional.of(new Employer()));
+            mockedFileUtil.when(() -> util.FileUtil.processSalaryCsvFiles(tempFile)).thenReturn(mockResult);
+
+            // Inject the directory path into the service
+            ReflectionTestUtils.setField(salaryService, "directoryPath", tempDir.toString());
+
+            salaryService.processSalaryCsvFilesJob();
+            mockedFileUtil.verify(() -> util.FileUtil.processSalaryCsvFiles(tempFile));
+        }
+    }
 }
